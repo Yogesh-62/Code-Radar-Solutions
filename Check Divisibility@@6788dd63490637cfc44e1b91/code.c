2fac973915b1656974cdd0d0485cadaf60597c6d
YOGESH <hoodayogesh85@gmail.com>
2025-03-10 17:08:14 +0530

    Solution to Check Divisibility

---
+++ Check Divisibility@@6788dd63490637cfc44e1b91/code.c
@@ -0,0 +1,9 @@
+// Your code here...
+#include <stdio.h>
+
+int main(){
+    int number;
+    scanf("%d\n", &number);
+    (number%5==0 && number%11==0) ? printf("Divisible\n") : printf("Not Divisible\n");
+    return 0;
+}
